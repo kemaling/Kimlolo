045512d572cec3c4be711b5fd9c384818bea8c43
kemaling <42378121+kemaling@users.noreply.github.com>
2024-02-25 15:56:43 +0700

    Code

---
+++ code.py
@@ -0,0 +1,7 @@
+print("tutorial perkalian untuk pemula")
+print("hello gayss")
+
+a = int(input("masukkan angka pertama : "))
+b = int(input("masukkan angka kedua : "))
+c = a + b
+print("hasil dari :",a,"+",b,"=",c,)
